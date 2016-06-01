f6d63d24e8a7d944b875a3b11cac50859a2f8b2d
abcautomate <abcautomate@gmail.com>
2016-06-01 10:29:28 +0530

    Update Class Name

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class OpenBrowser{
+public class HelloWorld{
 
 	public static void main(String args[]) throws Exception{
 
